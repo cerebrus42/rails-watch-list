6f9a9336091f51c744361701372dbd25f35b05a3
Kai Goedecke <kai@cerebrus.de>
2022-06-20 14:53:38 +0200

    copy css and styles from solution

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :find_list, only: %i[show]
+  before_action :find_list, only: %i[show destroy]
 
   def index
     @lists = List.all
@@ -20,6 +20,11 @@ class ListsController < ApplicationController
   end
 
   def show; end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
 
   private
 
